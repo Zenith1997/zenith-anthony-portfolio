b0e5fbbc2d18b6ef0220b0e578277cab1e4a28d7
Zenith1997 <zenithanthony.official@gmail.com>
2025-06-19 01:54:20 +0530

    after custom domain base fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-  <BrowserRouter basename="/zenith-anthony-portfolio">
+  <BrowserRouter basename="/">
   <Routes>
     <Route path="" element={<Index />} />
     <Route path="*" element={<NotFound />} />
